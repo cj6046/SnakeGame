611690028353e49b450b909b43cc994564505fc2
cj6046 <cdjones6046@gmail.com>
2021-08-10 17:57:40 -0400

    Setup the SnakePanel Class
    Added all of the methods I should need to it and included constructors for everything.

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -1,13 +1,58 @@
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JPanel;
+import java.util.Random;
 
 public class SnakePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+
+    SnakePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void spawnApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         
+    }
+
+    public class myKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
     
 }
