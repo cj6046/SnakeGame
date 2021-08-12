d7731731d7f36498d7a22c7220009dbc82e98036
cj6046 <cdjones6046@gmail.com>
2021-08-12 11:04:26 -0400

    Draw method and spawnApple method
    Added implementation to the draw and spawnApple methods. Also fixed the Timer import, as it was util.Timer, not the swing.Timer. Began work on the move method.

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.util.Random;
-import java.util.Timer;
 
 /**
  * This is the Panel class that holds all of the game logic
@@ -15,7 +15,7 @@ public class SnakePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
     final int[] x = new int[GAME_UNITS];
     final int[] y = new int[GAME_UNITS];
@@ -31,26 +31,44 @@ public class SnakePanel extends JPanel implements ActionListener {
     SnakePanel() {
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new myKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
-        
+        spawnApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g) {
-
+        // Drawing grid lines
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        // Drawing apple
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
 
     public void move() {
+        for (int i = bodyParts; i > 0; i--) {
 
+        }
     }
 
     public void spawnApple() {
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
 
     public void checkCollisions() {
