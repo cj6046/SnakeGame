2b766f4b55d17c058304fb71877303c8a391e210
cj6046 <cdjones6046@gmail.com>
2021-08-12 17:08:14 -0400

    Updated move(), draw(), and checkCollisions(). Added checkApple() and commenting on code.
    I did most of the move() method and added the snake to the draw() method. In the actionPerformed() method, I added a running if and called the move(). I also added the checkApple method, without any implementation and added implementation to the checkCollisions() method to see if the snake eats itself or goes off screen. Finally, I also added sufficient comments to most of my code to improve readability. A good habit to grow.

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -28,6 +28,9 @@ public class SnakePanel extends JPanel implements ActionListener {
     Timer timer;
     Random random;
 
+    /**
+     * This constructs the SnakePanel object
+     */
     SnakePanel() {
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -37,6 +40,9 @@ public class SnakePanel extends JPanel implements ActionListener {
         startGame();
     }
 
+    /**
+     * This class starts the game by initializing running to true
+     */
     public void startGame() {
         spawnApple();
         running = true;
@@ -44,11 +50,23 @@ public class SnakePanel extends JPanel implements ActionListener {
         timer.start();
     }
 
+    /**
+     * This method comes from the parent class
+     * and calls the draw() method
+     * 
+     * @param g The instance of Graphics class we are using
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
+    /**
+     * This method continuously draws the game
+     * and actually puts the apple from spawnApple on the game board
+     * 
+     * @param g The instance of Graphics class we are using
+     */
     public void draw(Graphics g) {
         // Drawing grid lines
         for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
@@ -58,34 +76,111 @@ public class SnakePanel extends JPanel implements ActionListener {
         // Drawing apple
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-    }
 
-    public void move() {
-        for (int i = bodyParts; i > 0; i--) {
-
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(new Color(45, 150, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
         }
     }
 
+    /**
+     * This method moves the snake along a vertical or horizontal path
+     * and controls the switching of direction
+     */
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        // Switch the direction of the snake
+        switch (direction) {
+            case ('R') :
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+            case ('L') :
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case ('U') :
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case ('D') :
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+        }
+    }
+
+    /**
+     * This method sets a random location for an apple to spawn
+     * but does NOT draw the apple in game
+     */
     public void spawnApple() {
         appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
         appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
 
-    public void checkCollisions() {
+    /**
+     * This method checks to see if the snake has eaten an apple
+     */
+    private void checkApple() {
 
     }
 
+    /**
+     * This method checks to see if the snake has gone off the screen
+     * or collided with itself
+     */
+    public void checkCollisions() {
+        // Check self-collision
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[i] == x[0]) && y[i] == y[0]) {
+                running = false;
+            }
+        }
+        // Check out of bounds collisions
+        if (x[0] > SCREEN_WIDTH || x[0] < 0) {
+            running = false;
+        }
+        if (y[0] > SCREEN_HEIGHT || y[0] < 0) {
+            running = false;
+        }
+    }
+
+    /**
+     * This method handles game over conditions
+     * and draws over the game board
+     * 
+     * @param g The instance of the Graphics class we are using
+     */
     public void gameOver(Graphics g) {
 
     }
 
+    /**
+     * This class handles what actions are performed each frame
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
 
+    /**
+     * Internal class for getting user input
+     */
     public class myKeyAdapter extends KeyAdapter {
+        /**
+         * This class gets the key input from the user
+         */
         @Override
         public void keyPressed(KeyEvent e) {
 
